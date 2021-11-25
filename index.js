2e3c21cfa720082e60b3435618a1d0a39953993f
Telphy <frazaokzn@hotmail.com>
2021-11-25 12:41:28 -0100

    update

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 const express = require('express')
 const path = require('path')
-const mysql = require('mysql2')
-require ('dotenv').config({path: 'private/.env'})
 const app = express()
+const connection = require('./dbconnection')
 
 app.use(express.static('./public'))
 
-const connection = mysql.createConnection({
-    host:process.env._HOST,
-    user:process.env._USER,
-    password:process.env._PASSWORD,
-    database:process.env._DATABASE,
-    port:process.env._PORT,
-})
 
-connection.connect(function(err){
-    if(err){
-        throw err;
-    }
-    else {
-        console.log('connected to database = ' + process.env._DATABASE)
-        
-    }
-})
 //localhost:5000/bd
 app.get('/bd', function (req, res) {
     connection.query('select * from city', function(err,result){
@@ -40,7 +23,7 @@ app.get('/', function (req, res) {
     es.sendFile(path.join(__dirname, './public/index.html'))
   })
  
-const port = 5000
+const port = 5000   
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
--- public/scripts.js
+++ public/scripts.js
@@ -1,12 +1,54 @@
 function getData(){
     fetch('http://localhost:5000/bd')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => processData(data) )
     .catch(function(err){
         alert('Ocorreu problema')
     })
 }
 
 function processData(data){
-    console.log(data)
-}
+        const linhaCidades = document.getElementById('cidades')
+        linhaCidades.innerHTML=''
+        for(let i=0; i < 10; i++){
+            let nome = data[i].Name
+            let distrito = data[i].District
+            let pop = data[i].Population
+            let id = data[i].ID
+            linhaCidades.innerHTML += ` <tr>
+                                            <th>${nome}</th>
+                                            <th>${distrito}</th>
+                                            <th>${pop}</th>
+                                            <td">
+                                            <button 
+                                            onclick="Delete"
+                                            type="button" 
+                                            class="btn btn-danger"
+                                            style="width:100px;>
+                                            Eliminar</button>
+
+                                            <button 
+                                            type="button" 
+                                            class="btn btn-success"
+                                            style="width:100px;>
+                                            Editar</button>
+
+                                            <button 
+                                            onclick="ShowID(${ID})"
+                                            type="button" 
+                                            class="btn btn-info"
+                                            style="width:100px;>
+                                            Ver mais</button>
+
+                                            </td>
+                                        </tr>`
+        }
+    }
+
+    function showID(ID){
+        console.log(ID)
+
+    }
+
+
+
